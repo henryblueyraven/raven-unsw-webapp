26bcd37525e500d2cca58444bc96ca5320d1a7aa
Henry Blumentals <>
2018-11-29 19:32:50 +1100

    Added Live Data Table with Conditonal Formatting

--- FlaskWeb/views.py
+++ FlaskWeb/views.py
@@ -6,8 +6,9 @@ from datetime import datetime
 from flask import render_template
 from FlaskWeb import app
 import pyodbc
-import pandas
+import pandas as pd
 import numpy as np
+import datetime as dt
 
 
 def sql():
@@ -18,14 +19,22 @@ def sql():
     driver= '{ODBC Driver 13 for SQL Server}'
     cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     sql = "SELECT * FROM analytics.Measurements ORDER BY timestamp ASC"
-    data = pandas.read_sql(sql,cnxn)
+    data = pd.read_sql(sql,cnxn)
+    data['timestamp'] =  pd.to_datetime(data['timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
+    # Convert from UTC to AEST
+    data.index = data['timestamp']
+    data['timestamp'] = (data.index.tz_localize('UTC').tz_convert('Australia/Sydney').strftime("%Y-%m-%d %I:%M %p"))
+    
     group = data.groupby(['deviceId']).agg(['count'])
-    liveval = data.drop_duplicates(['deviceId'], keep='last', inplace=False).reset_index(drop=True)
+    liveval = data.drop_duplicates(['deviceId','measurementDefinition'], keep='last', inplace=False).reset_index(drop=True)
     liveval = liveval.pivot_table(
             values='numericValue', 
             index=['deviceId', 'timestamp'], 
             columns='measurementDefinition', 
             aggfunc=np.sum)
+    liveval = pd.DataFrame(liveval.to_records())
+    liveval['state']= np.where(liveval['state'] == 1, 'Closed and Attended', np.where(liveval['state'] == 2, 'Open and Attended', np.where(liveval['state'] == 3, 'Open and Unattended', 'Closed and Unattended')))
+
     return group, liveval
 
 
@@ -38,7 +47,7 @@ def home():
     table1 = group.to_html(classes=['table', 'table-hover', 'table-bordered', 'thead-dark'], 
            float_format=lambda x: '{0:.3f}'.format(x))
     table2 = liveval.to_html(classes=['table', 'table-hover', 'table-bordered', 'thead-dark'], 
-           float_format=lambda x: '{0:.3f}'.format(x))
+           float_format=lambda x: '{0:.3f}'.format(x), table_id='livetable')
     return render_template(
         'index.html',
         title='Home Page',
