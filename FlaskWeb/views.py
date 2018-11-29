e3ce5be4202ddcd7b5661fb4882608e9b3e71339
Henry Blumentals <>
2018-11-29 14:13:00 +1100

    Added DB Connection in Again
    Added DB connection with DataFrames

--- FlaskWeb/views.py
+++ FlaskWeb/views.py
@@ -5,15 +5,46 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWeb import app
+import pyodbc
+import pandas
+import numpy as np
+
+
+def sql():
+    server = 'ravenunswpilot.database.windows.net'
+    database = 'continuousDataExportDB'
+    username = 'ravenadmin'
+    password = '''fe'Pc9\C3)_@}'yL'''
+    driver= '{ODBC Driver 13 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    sql = "SELECT * FROM analytics.Measurements ORDER BY timestamp ASC"
+    data = pandas.read_sql(sql,cnxn)
+    group = data.groupby(['deviceId']).agg(['count'])
+    liveval = data.drop_duplicates(['deviceId'], keep='last', inplace=False).reset_index(drop=True)
+    liveval = liveval.pivot_table(
+            values='numericValue', 
+            index=['deviceId', 'timestamp'], 
+            columns='measurementDefinition', 
+            aggfunc=np.sum)
+    return group, liveval
+
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    group, liveval  = sql()
+    table1 = group.to_html(classes=['table', 'table-hover', 'table-bordered', 'thead-dark'], 
+           float_format=lambda x: '{0:.3f}'.format(x))
+    table2 = liveval.to_html(classes=['table', 'table-hover', 'table-bordered', 'thead-dark'], 
+           float_format=lambda x: '{0:.3f}'.format(x))
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        table1 = table1, 
+        table2 = table2
     )
 
 @app.route('/contact')
